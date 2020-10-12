66847e6fff9443ebcb5082e197d33b1399036cf6
Philip Withnall <pwithnall@endlessos.org>
2020-10-12 14:40:28 +0100

    app-info: Add flatpak bundle IDs to eos-apps script
    See https://gitlab.gnome.org/GNOME/gnome-software/-/merge_requests/521.
    The appstream format requires the content of a `<bundle/>` element to be
    set, and gnome-software emits a critical warning otherwise.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    
    https://phabricator.endlessm.com/T30217

--- app-info/generate_endless_apps_popular_xml.sh
+++ app-info/generate_endless_apps_popular_xml.sh
@@ -2,13 +2,17 @@
 
 # Refer https://phabricator.endlessm.com/w/software/gnome-software/featured-apps/ for documentation.
 
-for app in $(flatpak remote-ls eos-apps --app --columns=application | grep "com.endless");
+IFS=$'\n'; for row in $(flatpak remote-ls eos-apps --app --columns=application,ref | grep "com.endless");
 do
+	IFS=$'\t '
+	cells=($row)
+	app_id=${cells[0]}
+	ref=${cells[1]}
 	cat <<< '<component type="desktop" merge="append">'
-	cat <<< '<id>'$app'</id>'
+	cat <<< '<id>'$app_id'</id>'
 	cat <<< '<kudos>'
 	cat <<< '<kudo>GnomeSoftware::popular</kudo>'
 	cat <<< '</kudos>'
-	cat <<< '<bundle type="flatpak"/>'
+	cat <<< '<bundle type="flatpak">'$ref'</bundle>'
 	cat <<< '</component>'
 done
